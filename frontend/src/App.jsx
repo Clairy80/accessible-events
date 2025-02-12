c2036fccabd969855a4136f1e86a2d3e61513ba1
Claire <claire.solf@dci-student.org>
2025-02-12 11:49:31 +0100

    bis hier hin klappt alles

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Barrierefreie Events</h1>
+      <p>Willkommen zu deinem Event-Portal.</p>
+    </div>
+  );
+}
+
+export default App;
