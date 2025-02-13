b61e71b5f09bb7a332cfd22a02d4e80be88f6f73
Claire <claire.solf@dci-student.org>
2025-02-13 16:16:47 +0100

    soweit es ging das Backend mit dem Frontend verbunden

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("Hallo Welt");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+  app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+}).catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
